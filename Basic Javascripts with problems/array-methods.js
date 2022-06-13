b9e228121e195b20b8762298630cffbe77f80d8e
MirzaAyon <ayonmirza98@gmail.com>
2022-06-14 01:49:10 +0600

    array-methods added

---
+++ Basic Javascripts with problems/array-methods.js
@@ -0,0 +1,10 @@
+var arr = [10, 15, 20, 25, 30, 35, 40, 43, 75, 69];
+
+var addOneNumber = arr.push(120); // last e data add kore
+
+arr.unshift(5); // first e data add kora
+
+arr.pop(); // last e data remove kore
+
+arr.shift(); // first e data remove kore
+arr.shift(); // first e data remove kore
