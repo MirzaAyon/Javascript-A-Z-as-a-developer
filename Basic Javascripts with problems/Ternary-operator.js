c493afc3f6bdd09ef32754f3ef8867d251f959bf
MirzaAyon <ayonmirza98@gmail.com>
2022-06-14 01:42:38 +0600

    ternary operator added

---
+++ Basic Javascripts with problems/Ternary-operator.js
@@ -0,0 +1,8 @@
+// Ternary operator
+var number1 = 1000;
+var number2 = 2000;
+//  var test = (condition) ? "true" : "false";
+var result = (number1 > number2) ? "Number1 is big" : "Number2 is big";
+//ekhane ? and : ei 2 ta condition er jonno use hoise
+
+console.log(result);
