4c1e084df445596c09dd1e96591d08947941ea39
MirzaAyon <ayonmirza98@gmail.com>
2022-06-14 00:36:20 +0600

    object and array added

---
+++ Javascript fundamentals/object.js
@@ -0,0 +1,20 @@
+var student1 = {
+    firstName: "Ayon",
+    lastName: "Plabon",
+    age: 50,
+    hobby: ["h1", "h2"],
+};
+
+var student2 = {
+    firstName: "Sami",
+    lastName: "Riya",
+    age: 30,
+};
+
+var numbers = [1, 2, 3, 4, 5, 6];
+console.log(numbers);
+
+var studentsCollection = [student1, student2];
+console.log(studentsCollection);
+console.log(student1.hobby);
+console.log(student1.hobby[0]);
