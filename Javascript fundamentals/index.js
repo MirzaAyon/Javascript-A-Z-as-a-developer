c4b9f5d7e0602f09667545f3b628ee65c325a4f2
MirzaAyon <ayonmirza98@gmail.com>
2022-06-14 00:27:43 +0600

    variable, number,array,string,boolean added

---
+++ Javascript fundamentals/index.js
@@ -0,0 +1,29 @@
+//variable
+//string
+var myName = "Ayon";
+console.log(myName);
+//number
+var number = 10;
+console.log(number);
+
+//variable without var keyword
+age = "2.50";
+console.log(age);
+console.log(parseFloat(age))
+
+//array
+var myFriends = ["abc", "xyz", 20, 23];
+console.log(myFriends);
+
+
+
+var num1 = 40.5;
+var num2 = "85";
+console.log(num1, num2);
+
+var isRaining = false;
+console.log(isRaining);
+
+var frndName;
+frndName = "someone";
+console.log(frndName)
