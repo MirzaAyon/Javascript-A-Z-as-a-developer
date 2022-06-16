37d181fc53daee40155a1c439a654fb37190d464
MirzaAyon <ayonmirza98@gmail.com>
2022-06-17 00:16:01 +0600

    first and 2nd problem added

---
+++ Applying more Javascripts/message-generator.js
@@ -0,0 +1,8 @@
+//2. Write a simple function that takes name as a parameter  and then return a custom message.
+
+function msgGenerator(name) {
+    var message = 'Hello ' + name + ', Welcome to Programming Hero'
+    return message
+}
+
+console.log(msgGenerator('Mirza Plabon'));
---
+++ Applying more Javascripts/sum-of-two-numbers.js
@@ -0,0 +1,8 @@
+// Sum of 2 numbers
+
+function sumOfNumbers(num1, num2) {
+    var result = num1 + num2
+    console.log(result)
+    return result
+}
+console.log(sumOfNumbers(65, 4));
