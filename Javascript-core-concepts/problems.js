be8472b886f92b3b1342400030b45e3c8cd0b719
MirzaAyon <ayonmirza98@gmail.com>
2022-07-02 09:51:09 +0600

    another problem added

--- Javascript-core-concepts/problems.js
+++ Javascript-core-concepts/problems.js
@@ -6,3 +6,15 @@ for (var i = 0; i <= 10; i++) {
         console.log(i)
     }
 }
+
+//No - 2
+function posNeg(num1, num2) {
+    if (num1 > 0 && num2 < 0) {
+        console.log('correct input');
+    }
+    else {
+        console.log('incorrect input')
+    }
+}
+posNeg(3, 8)
+posNeg(3, -8)
